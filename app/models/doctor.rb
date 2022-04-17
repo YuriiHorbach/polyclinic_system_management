3ef14588026f95bac8328ca70037f90b4d5a09fb
Yurii Horbach <yuriihorbach@Yuriis-MacBook-Air.local>
2022-04-17 14:11:14 +0300

    add function full_name for Doctors model and add routes

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,9 +1,13 @@
 class Doctor < ApplicationRecord
-  has_many :apointments 
+  has_many :apointments, dependent: :destroy
   has_many :patients, through: :apointments
 
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :patient, length: {maximum: 10}
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,7 +1,11 @@
 class Patient < ApplicationRecord
-  has_many :apointments 
+  has_many :apointments, dependent: :destroy
   has_many :doctors, through: :apointments
 
   validates :first_name, presence: true
   validates :last_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
